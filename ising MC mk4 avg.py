440fddcc5e6187143637af8ad4f6d38d59e932ce
Harrison Parker <harrison-parker@outlook.com>
2024-11-05 12:23:47 +0000

    Organising and updated ising 4

--- ising MC mk4 avg.py
+++ ising MC mk4 avg.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-L=8
+L=16
 T=0.1
-iterations = 5000
-avgpool = 30
+iterations = 6000
+avgpool = 20
+0
+kb= 1#.380649e-23
 templist = []
 deltaElist = []
 energylist = []
@@ -13,11 +15,12 @@ probacceptlist = []
 randomlist = []
 magerrorlist = []
 
-for i in range(78):
+for t in range(78):
     templist.append(T)
+    Magnetisationavglist = []
     for a in range(avgpool):
         # for a ferromagnetic system the coupling constant is positive:
-        J=-1
+        J=1#.60218e-19
 
         # create a random spin configuration
         # the possible values of the spins are +1 or -1
@@ -31,11 +34,11 @@ for i in range(78):
                 for j in range(L):
                     S = spins[i,j]
                     # this sums the neighbors with periodic boundary conditions by using the modulo operator % to wrap around the edges
-                    nb = spins[(i+1)%L, j] + spins[i,(j+1)%L] + spins[(i-1)%L, j] + spins[i,(j-1)%L]
+                    neighbours = spins[(i+1)%L, j] + spins[i,(j+1)%L] + spins[(i-1)%L, j] + spins[i,(j-1)%L]
                     #instead of summing neighbor times spin ij, we sum all neighbors and multiply by the spin ij
-                    energy += -nb*J*S
+                    energy += -neighbours*J*S
 
-            # Divide by 2 to correct for double-counting the pairwise interactions
+            # Divide by 2 to correct for double-counting
             energy /= 2
             #gonna flip a random spin
             i = np.random.randint(L)
@@ -50,10 +53,10 @@ for i in range(78):
             for i in range(L):
                 for j in range(L):
                     S = new_spins[i,j]
-                    nb = new_spins[(i+1)%L, j] + new_spins[i,(j+1)%L] + new_spins[(i-1)%L, j] + new_spins[i,(j-1)%L]
-                    new_energy += -nb*J*S
+                    neighbours = new_spins[(i+1)%L, j] + new_spins[i,(j+1)%L] + new_spins[(i-1)%L, j] + new_spins[i,(j-1)%L]
+                    new_energy += -neighbours*J*S
 
-            # Divide by 2 to correct for double-counting the pairwise interactions
+            # Divide by 2 to correct for double-counting
             new_energy /= 2
             #calculate the energy difference
             deltaE = new_energy - energy
@@ -62,15 +65,16 @@ for i in range(78):
 
             #accept or reject the move
             if deltaE < 0:
+                probacceptlist.append(1)
                 spins = new_spins
                 energy = new_energy
                 #print("accepted <0")
             else:
                 #calculate the acceptance probability
-                # kb=1 for simplicity
-                p = np.exp(-deltaE/T)
+                p = np.exp(-deltaE/kb*T)
                 probacceptlist.append(p)
-                r = np.random.rand()
+                r=0.8
+                #r = np.random.rand()
                 randomlist.append(r)
                 #print("p ",p)
                 #print("r ",r)
@@ -78,20 +82,16 @@ for i in range(78):
                     spins = new_spins
                     energy = new_energy
                     #print("accepted by probability")
-                #if r > p:
-                    #print("rejected by probability")
             energylist.append(energy)
         magnetisation = np.abs(np.sum(spins)) * (1 / (L * L))
         Magnetisationavglist.append(magnetisation)
         print("magnetisation ",magnetisation," T ",T)
     Magnetisationlist.append(np.mean(Magnetisationavglist))
     magerrorlist.append(np.std(Magnetisationavglist))
-    T+=0.1
+    T+=0.5
 
-print("templlist length ",len(templist))
-print("Magnetisationlist length ",len(Magnetisationlist))
-print("magerrorlist length ",len(magerrorlist))
-#plot magnetisation against temperature
+print(spins)
+
 plt.errorbar(templist,Magnetisationlist,yerr=magerrorlist,fmt='o')
 plt.xlabel('Temperature')
 plt.ylabel('Magnetisation')
@@ -101,9 +101,3 @@ plt.figtext(0.15, 0.80, f'L = {L}', fontsize=12)
 plt.figtext(0.15, 0.75, f'iterations = {iterations}', fontsize=12)
 plt.figtext(0.15, 0.70, f'avgpool = {avgpool}', fontsize=12)
 plt.show()
-
-
-print("final E ",energy)
-print(spins)
-
-
